99c06eda7becde9bfd9a93b14b185ff958129a15
Gabriel Bourget <gabrielbourget@Gabriels-MacBook-Pro.local>
2023-07-16 16:48:12 -0400

    generateTabContent()

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -16,8 +16,25 @@ import Tab from "../components/Tab";
 
 const Customizer = () => {
   const snapshot = useSnapshot(state);
+  const [file, setFile] = useState("");
+  const [prompt, setPrompt] = useState("");
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeFilterTab, setActiveFilterTab] = useState({ logoShirt: true, stylishShirt: false });
 
-  
+
+  const generateTabContent = () => {
+    switch(activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />;
+      case "filepicker":
+        return <FilePicker />;
+      case "aipicker":
+        return <AIPicker />
+      default: return null;
+    }
+  };
+
 
   return (
     <AnimatePresence>
@@ -36,10 +53,12 @@ const Customizer = () => {
                       <Tab
                         key={tab.name}
                         tab={tab}
-                        handleClick={() => {}}
+                        handleClick={() => setActiveEditorTab(tab.name)}
                       />
                     ))
                   }
+
+                  { generateTabContent() }
                 </div>
               </div>
             </motion.div>
